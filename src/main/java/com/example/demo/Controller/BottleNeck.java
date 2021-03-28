567286e70c2b0c07f233a4d1ed133617aa4d4ef8
d10sama <d10sama@bupt.edu.cn>
2021-03-28 21:17:23 +0800

    feat: bottle neck

--- src/main/java/com/example/demo/Controller/BottleNeck.java
+++ src/main/java/com/example/demo/Controller/BottleNeck.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -27,16 +28,7 @@ public class BottleNeck {
     static long OriginNum=0;//记录原始条目数
     long presentNum;//记录当前条目数
     //sql语句
-    String sentence1 = "select id  from Product_Table order by id desc limit 0,1;";
-    String template1="select  line1_station2_product_time,line1_station3_product_time," +
-            "line1_station4_product_time, " +
-            "line1_station5_product_time, " +
-            "line1_station6_product_time, " +
-            "line1_station7_product_time, " +
-            "line1_station8_product_time, " +
-            "line1_station9_product_time, " +
-            "line1_station10_product_time"+
-            " from Product_Table where id=%d";
+    String sentence1 = "select *  from Product_Table order by id desc limit 0,1;";
     //选取database
     @Autowired
     @Qualifier("primaryJdbcTemplate")
@@ -44,9 +36,61 @@ public class BottleNeck {
 
     //compare whether new neck is generated
     @RequestMapping("/l1neck")//返回页面
-    @Scheduled(fixedRate = 1000)//定时1秒
-    public List<Map<String, Object>> content1() {
-        return null;
+    @Scheduled(fixedRate = 100000)//定时1秒
+    public Map<String, Object> content1() {
+        int count=1;
+        LinkedHashMap<String,Object> temp=new LinkedHashMap<String,Object>();
+        List<Map<String,Object>> result=jdbcTemplate1.queryForList(sentence1);
+        for(Map<String, Object> map:result)
+            for(String s: map.keySet())
+            {
+                if(count==1)
+                {temp.put(s,map.get(s).toString());}
+                if(count>=16&&count<=22) {
+                    temp.put(s,map.get(s).toString());
+                }
+                count+=1;
+            }
+        return cmp(temp);
+    }
+    private LinkedHashMap<String,Object> cmp(LinkedHashMap<String,Object> tmp)
+    {
+        int count=2;
+        int idignore=0;
+        int value=0;
+        int tmpvalue;
+        final String Neck="Neck";
+        for(String s:tmp.keySet())
+        {
+            if(idignore!=0) {
+                tmpvalue=Integer.parseInt(tmp.get(s).toString());
+                if(value<tmpvalue)
+                {
+                    value = tmpvalue;
+                    count++;
+                    System.out.println(s+" "+"value="+value+" count="+count);
+                }
+            }
+            idignore++;
+        }
+
+        switch (count)
+        {
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+                break;
+            case 6:
+            case 7:
+                count++;
+                break;
+            case 8:
+                count+=2;
+                break;
+        }
+        tmp.put(Neck,count);
+        return tmp;
     }
 
 }
