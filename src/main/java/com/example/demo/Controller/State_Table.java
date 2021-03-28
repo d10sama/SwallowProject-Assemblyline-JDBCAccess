36ae1e15895941f09136ccae67489f32c745041a
d10sama <d10sama@bupt.edu.cn>
2021-03-28 12:51:37 +0800

    feat: state_table

---
+++ src/main/java/com/example/demo/Controller/State_Table.java
@@ -0,0 +1,67 @@
+package com.example.demo.Controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+@ResponseBody
+@Controller
+@CrossOrigin
+public class State_Table {
+    //sql语句
+    final private String command="select * from State_Table limit 0,1;";
+
+
+    //选取设定好的主database
+    @Autowired
+    @Qualifier("primaryJdbcTemplate")
+    private JdbcTemplate jdbcTemplate1;
+
+    @RequestMapping("/states")
+    @ResponseBody
+    @Scheduled(fixedRate = 5000)
+    public LinkedHashMap<String,Object> contentLoad()
+    {
+        int count=1;
+
+        List<Map<String,Object>> result=jdbcTemplate1.queryForList(command);
+        LinkedHashMap<String,Object> tmp=new LinkedHashMap<String,Object>();
+        for(Map<String,Object> map:result)
+            for(String s:map.keySet())
+            {
+                if(count<=13)
+                {   tmp.put(s,map.get(s).toString());}
+                else if(count==14)
+                {
+                    String tempstring=map.get(s).toString();
+                    for(int i=0;i<16;i++)
+                    {
+
+                        tmp.put(String.format("sc%d",i+1),tempstring.charAt(i));
+                    }
+                }
+                else
+                {
+                    String tempstring=map.get(s).toString();
+                    for(int i=0;i<16;i++)
+                    {
+                        tmp.put(String.format("se%d",i+1),tempstring.charAt(i));
+                    }
+                }
+                count++;
+            }
+        return tmp;
+    }
+
+
+}
--- src/main/java/com/example/demo/Controller/sample1_query.java
+++ src/main/java/com/example/demo/Controller/sample1_query.java
@@ -21,28 +21,29 @@ public class sample1_query {
     private static final String template = "select * from Product_Table order by id desc limit 0,1;";
 
     private static final String sentence2="select id  from Product_Table order by id desc limit 0,1;";
-    String[] NGOK;
     int preID;
+    LinkedHashMap<String,Object> temp;
     //选取设定好的主database
     @Autowired
     @Qualifier("primaryJdbcTemplate")
     private JdbcTemplate jdbcTemplate1;
     //用于返回表中对象
-    @RequestMapping("/PD_Table")
+    @RequestMapping("/sample1")
     @Scheduled(fixedRate = 500000)//定时1秒
     @ResponseBody
     public List<Map<String,Object>> contextLoads() {
         int c=0;
         List<Map<String,Object>> result=jdbcTemplate1.queryForList(template);
         int count=1;
-        this.NGOK=new String[28];
+        this.temp=new LinkedHashMap<String,Object>();
         for(Map<String,Object> map:result)
             for(String s: map.keySet())
             {
+                if(count==1)
+                    this.temp.put(s,map.get(s).toString());
                 if(count>25&&count<54) {
                     //System.out.println(s+":"+map.get(s).toString()+" "+c);
-                    this.NGOK[c]=map.get(s).toString();
-                    c++;
+                    this.temp.put(s,map.get(s).toString());
                 }
                 count++;
             }
@@ -60,27 +61,7 @@ public class sample1_query {
     @RequestMapping("/PASS_OK_NG")
     public Map<String, Object> qualified()
     {
-        Map<String,Object> temp=new LinkedHashMap<String,Object>();
-        //若all_ok等于true则跳过判断
-        if(this.NGOK[18]=="true")
-        {
-            temp.put("allok",1);
-        }else {
-            temp.put("allok",0);
-            for (int i = 0; i < 9; i++) {
-                //否则判断(PASS为真且NG为真）的工位
-                if (this.NGOK[i] == "true" && this.NGOK[i + 9] == "true") {
-                    temp.put(String.format("line1_station%d_product_OK", i), 1);
-                }
-            }
-            for (int i = 0; i < 9; i++)
-            {
-                if (this.NGOK[i] == "true" && this.NGOK[i + 19] == "false") {
-                    temp.put(String.format("line1_station%d_product_NG", i), 0);
-                }
-            }
-        }
-        return temp;
+        return this.temp;
     }
 
 }
