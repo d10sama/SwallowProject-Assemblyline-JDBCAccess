cd054942cf55fc03d52bb6bd05cea0c606c29b0a
d10sama <d10sama@bupt.edu.cn>
2021-03-28 21:44:56 +0800

    feat: time percent

---
+++ src/main/java/com/example/demo/Controller/TimeConsume.java
@@ -0,0 +1,58 @@
+package com.example.demo.Controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.List;
+
+
+@ResponseBody
+@Controller
+@CrossOrigin
+public class TimeConsume {
+    final String query50="select time_difference from Product_Table order by id desc limit 0,50;";
+    //时间统计，数组顺序代表自己到自己加十，左闭右开，如time[7]=70（含）~80（不含）
+    int times[];
+
+    //SELECT TABLE
+    @Autowired
+    @Qualifier("primaryJdbcTemplate")
+    private JdbcTemplate jdbcTemplate1;
+
+    @RequestMapping("/recent50")
+    @Scheduled(fixedRate = 1000)
+    public LinkedHashMap<String,Object> timeconsume()
+    {
+        List<Map<String,Object>> result=jdbcTemplate1.queryForList(query50);
+        int count=result.size();
+        int tmpvalue;
+        int times[]=new int[11];
+        for(int i=0;i<11;i++)
+            times[i]=0;
+        for(Map<String,Object> map:result) {
+            for (String s : map.keySet()) {
+                tmpvalue = Integer.parseInt(map.get(s).toString()) / 10;
+                times[tmpvalue / 10]++;
+            }
+        }
+        LinkedHashMap<String,Object> fin=new LinkedHashMap<String,Object>();
+        int totaltimes=0;
+        for(int i:times)
+            totaltimes+=i;
+        for(int i:times)
+        {
+            fin.put("0",i/totaltimes);
+        }
+        return fin;
+    }
+
+}
