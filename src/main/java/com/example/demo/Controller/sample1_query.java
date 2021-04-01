f6d59c9126c16dc8da58da09844f930688150705
d10sama <d10sama@bupt.edu.cn>
2021-04-01 18:15:30 +0800

    formating

--- src/main/java/com/example/demo/Controller/sample1_query.java
+++ src/main/java/com/example/demo/Controller/sample1_query.java
@@ -24,70 +24,8 @@ public class sample1_query {
     private static final String template = "select * from Product_Table order by id desc limit 0,1;";
 
     private static final String sentence2="select id  from Product_Table order by id desc limit 0,1;";
+
     LinkedHashMap<String,Object> temp;
-    //选取设定好的主database
-    @Autowired
-    @Qualifier("primaryJdbcTemplate")
-    private JdbcTemplate jdbcTemplate1;
-    //用于返回表中对象
-    @RequestMapping("/sample1")
-    @Scheduled(fixedRate = 500)//定时0.5秒
-    @ResponseBody
-    public List<Map<String,Object>> contextLoads() {
-        int c=0;
-        List<Map<String,Object>> result=jdbcTemplate1.queryForList(template);
-        int count=1;
-        this.temp=new LinkedHashMap<String,Object>();
-        for(Map<String,Object> map:result)
-            for(String s: map.keySet())
-            {
-                if(count==1)
-                    this.temp.put(s,map.get(s).toString());
-                if(count>25&&count<54) {
-                    this.temp.put(s,map.get(s).toString());
-                }
-                count++;
-            }
-        return result;
-    }
-    @RequestMapping("/PASS_OK_NG")
-    public Map<String, Object> qualified()
-    {
-        return this.temp;
-    }
-
-    //接收合格品数，并自我运算完成产品数，再存入数据库中
-    final private String command_qual="update statistics_table set qualified_product_number=%d where id=1;";
-    final private String command_fini="update statistics_table set finished_product_number=%d where id=1;";
-    //
-    @RequestMapping(value="/qualified")//,method={RequestMethod.POST,RequestMethod.GET})
-    @Scheduled(fixedRate = 500)
-    @ResponseBody
-    public LinkedHashMap<String, Object> Adapter()//HttpServletRequest request, HttpSession session)
-    {
-        int qualified=0, completed=0;
-
-        List<Map<String,Object>> result=jdbcTemplate1.queryForList("select product_total_ok from Product_Table");
-        int count=1;
-        this.temp=new LinkedHashMap<String,Object>();
-        for(Map<String,Object> map:result)
-            for(String s: map.keySet())
-            {
-                if((boolean)map.get(s)==true)
-                {
-                    qualified++;
-                }
-            }
-        completed=jdbcTemplate1.queryForObject("select id from Product_Table order by id desc limit 0,1;",Integer.class);
-        //执行sql语句
-        jdbcTemplate1.execute(String.format(command_qual,qualified));
-        jdbcTemplate1.execute(String.format(command_fini,completed));
-        LinkedHashMap<String,Object> outcome=new LinkedHashMap<>();
-        outcome.put("qualified",qualified);
-        outcome.put("finished",completed);
-        outcome.put("qualified_persentage",(double)qualified/completed);
-        return outcome;
-    }
 
     final private String Select_line_product_Id="select line_product_Id from Product_Table order by id desc limit 0,1;";
     final private String getOnlineYear="select online_time_year from Product_Table order by id desc limit 0,1;";
@@ -131,6 +69,70 @@ public class sample1_query {
             "spc_line1_station070_result_9,"+
             "spc_line1_station070_result_10"+
             " from Product_Table order by id desc limit 0,50;";
+
+    final private String command_qual="update statistics_table set qualified_product_number=%d where id=1;";
+    final private String command_fini="update statistics_table set finished_product_number=%d where id=1;";
+
+    //选取设定好的主database
+    @Autowired
+    @Qualifier("primaryJdbcTemplate")
+    private JdbcTemplate jdbcTemplate1;
+    //用于返回表中对象
+    @RequestMapping("/sample1")
+    @Scheduled(fixedRate = 500)//定时0.5秒
+    @ResponseBody
+    public List<Map<String,Object>> contextLoads() {
+        int c=0;
+        List<Map<String,Object>> result=jdbcTemplate1.queryForList(template);
+        int count=1;
+        this.temp=new LinkedHashMap<String,Object>();
+        for(Map<String,Object> map:result)
+            for(String s: map.keySet())
+            {
+                if(count==1)
+                    this.temp.put(s,map.get(s).toString());
+                if(count>25&&count<54) {
+                    this.temp.put(s,map.get(s).toString());
+                }
+                count++;
+            }
+        return result;
+    }
+    @RequestMapping("/PASS_OK_NG")
+    public Map<String, Object> qualified()
+    {
+        return this.temp;
+    }
+
+    @RequestMapping(value="/qualified")//,method={RequestMethod.POST,RequestMethod.GET})
+    @Scheduled(fixedRate = 500)
+    @ResponseBody
+    public LinkedHashMap<String, Object> Adapter()//HttpServletRequest request, HttpSession session)
+    {
+        int qualified=0, completed=0;
+
+        List<Map<String,Object>> result=jdbcTemplate1.queryForList("select product_total_ok from Product_Table");
+        int count=1;
+        this.temp=new LinkedHashMap<String,Object>();
+        for(Map<String,Object> map:result)
+            for(String s: map.keySet())
+            {
+                if((boolean)map.get(s)==true)
+                {
+                    qualified++;
+                }
+            }
+        completed=jdbcTemplate1.queryForObject("select id from Product_Table order by id desc limit 0,1;",Integer.class);
+        //执行sql语句
+        jdbcTemplate1.execute(String.format(command_qual,qualified));
+        jdbcTemplate1.execute(String.format(command_fini,completed));
+        LinkedHashMap<String,Object> outcome=new LinkedHashMap<>();
+        outcome.put("qualified",qualified);
+        outcome.put("finished",completed);
+        outcome.put("qualified_persentage",(double)qualified/completed);
+        return outcome;
+    }
+
     //KPI功能:显示单个产品相关数据
     @RequestMapping("/kpi1")
     @Scheduled(fixedRate = 500)
